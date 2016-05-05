03cf7a673e5a1f7644254aadec4b3bc8975c702b
teancake <das.xiaoke@gmail.com>
2016-05-05 23:24:31 +0800

    Change the SBK bit in CR1 together with the LINEN bit in CR2 to enable the sending of break frames on TX of a U(S)ART.

--- nuttx/arch/arm/src/stm32/stm32_serial.c
+++ nuttx/arch/arm/src/stm32/stm32_serial.c
@@ -1908,20 +1908,24 @@ static int up_ioctl(struct file *filep, int cmd, unsigned long arg)
 #ifdef CONFIG_USART_BREAKS
     case TIOCSBRK:  /* BSD compatibility: Turn break on, unconditionally */
       {
-        irqstate_t flags = irqsave();
-        uint32_t cr2 = up_serialin(priv, STM32_USART_CR2_OFFSET);
-        up_serialout(priv, STM32_USART_CR2_OFFSET, cr2 | USART_CR2_LINEN);
-        irqrestore(flags);
+	irqstate_t flags = irqsave();
+	uint32_t cr1 = up_serialin(priv, STM32_USART_CR1_OFFSET);
+	up_serialout(priv, STM32_USART_CR1_OFFSET, cr1 | USART_CR1_SBK);
+	uint32_t cr2 = up_serialin(priv, STM32_USART_CR2_OFFSET);
+	up_serialout(priv, STM32_USART_CR2_OFFSET, cr2 | USART_CR2_LINEN);
+	irqrestore(flags);
       }
       break;
 
     case TIOCCBRK:  /* BSD compatibility: Turn break off, unconditionally */
       {
-        irqstate_t flags;
+	irqstate_t flags;
         flags = irqsave();
-        uint32_t cr1 = up_serialin(priv, STM32_USART_CR2_OFFSET);
+	uint32_t cr1 = up_serialin(priv, STM32_USART_CR1_OFFSET);
+        up_serialout(priv, STM32_USART_CR1_OFFSET, cr1 & ~USART_CR1_SBK);
+	uint32_t cr2 = up_serialin(priv, STM32_USART_CR2_OFFSET);
         up_serialout(priv, STM32_USART_CR2_OFFSET, cr2 & ~USART_CR2_LINEN);
-        irqrestore(flags);
+	irqrestore(flags);
       }
       break;
 #endif
